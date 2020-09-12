bee807f16d2eccbb557bd03272a7cc37f6b63cce
MIDE-589 <temp8465245@hotmail.com>
2020-09-12 23:59:14 +0800

    import math

--- foobar.py
+++ foobar.py
@@ -1,4 +1,8 @@
+import math
 print("Hello World")
 for i in range(10,100,5):
     print("%d * %d = %d" % (i, i, i*i))
-
+a="I'm %(name)s. I'm %(age)d year old" % {'name':'Vamei', 'age':99}
+print(a)
+print(type(a))
+print(math.e)
